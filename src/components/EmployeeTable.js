47c53512171de1dca46eacfa1f68969e9b7478b6
Aftaab Dobani <1234dobani@gmail.com>
2021-05-15 12:33:45 -0400

    added table now need to edit it

--- src/components/EmployeeTable.js
+++ src/components/EmployeeTable.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
-import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 
-
-
-
-class EmployeeTable extends Component {
-
-}
+function EmployeeTable(props) {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            name="breed"
+            list="breeds"
+            type="text"
+            className="form-control"
+            placeholder="Type in a dog breed to begin"
+            id="breed"
+          />
+          <datalist id="breeds">
+            {props.breeds.map(breed => (
+              <option value={breed} key={breed} />
+            ))}
+          </datalist>
+          <button type="submit" onClick={props.handleFormSubmit} className="btn btn-success">
+            Search
+          </button>
+        </div>
+      </form>
+    );
+  }
+  
+  export default SearchForm;
